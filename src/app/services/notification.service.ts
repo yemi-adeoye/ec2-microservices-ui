df32f35a5573dd9bda189e47db4af2e65fc10f0f
yemi-adeoye <adeyemi_adeoye@hotmail.com>
2025-01-30 19:35:29 -0600

    Adds notification service

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { io, Socket } from 'socket.io-client'
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  readonly $notifications: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  getNotifications() {
+
+  }
+
+  createConnection = () => {
+    const socket = io("http://localhost:7000") // TODO import from environment file
+    return socket;
+  }
+
+  subscribe = (socket: Socket, channel: string, handler: Function) => {
+    socket.on(channel, (data) => {
+      handler(data)
+    })
+  }
+
+  sendMessage = (socket: Socket, channel: string, message: string) => {
+    socket.emit(channel, message)
+  }
 }
